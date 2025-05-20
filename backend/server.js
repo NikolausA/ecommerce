f9b515446975030b51c6da51fb0409ad1fd1f10d
NikolausA <nik.aleksenko@gmail.com>
2025-05-20 11:01:37 +0300

    fix: fix docker file

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 const routes = require("./routes");
 
 const app = express();
@@ -16,9 +17,11 @@ app.use("/uploads", express.static("uploads"));
 
 app.use("/api", routes);
 
-// app.get("/api", (req, res) => {
-//   res.json({ message: "Server is working" });
-// });
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => {
   app.listen(PORT, () => {
